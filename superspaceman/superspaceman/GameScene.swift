62c62a057c2e20d1e6b4cd7fc13ba423277f8d74
Afun Chen <Afun@promote.cache-dns.local>
2018-10-17 06:54:23 +0800

    Chapter 4 : - Adding another layer to the scene named foregroundNode for hold all the sprites that will affect game play, - Adding impulseCount the purpose this code is to put this game in initial start state with the player stationary until you tap the screen to start, When the screen is tapped the first time, the game begins.The impulseCount property was added to introduce a new game element. The game is going to use the impulseCount property to give the user a limited number of impulses that can be used for thrusts. The impulseCount property will increment each time the playerNode comes into contact with an orb and decrement every time the player taps the screen. This means the user must be good at collecting orbs, or they will eventually fall into oblivion and lose the game. - Adding more orbNode - Changing how orb contacts are handled. whenever the playerNode came into contact with an orbNode the orbNode was removed from the scene. This is still going to happen, but now the contact is going to also increment the impulseCount variable, giving the player additional impulses. The new didBeginContact() increases the impulseCount property each time the player comes into contact with an orb and then removes the orb. Now the player has additional fuel to keep from crashing into the planet’s surface below. - Add scrolling to game scene use update() method, changes the position of the backgroundNode based on the current position of the player. Specifically, it sets the position of the backgroundNode to its same x-value but uses a y-value that is 180 points below the position of the player, which is then divided by 8.  When you tap the screen this time, you will see the background slowly move toward the bottom of the scene as the player flies off the top of the screen. - Add accelerometer to move the player

--- superspaceman/superspaceman/GameScene.swift
+++ superspaceman/superspaceman/GameScene.swift
@@ -7,64 +7,117 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class GameScene: SKScene {
     let backgroundNode = SKSpriteNode(imageNamed: "Background")
     let playerNode = SKSpriteNode(imageNamed: "Player")
-    let orbNode = SKSpriteNode(imageNamed: "PowerUp")
-    
     let CollisionCategoryPlayer : UInt32 = 0x1 << 1
     let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
+    let foregroundNode = SKSpriteNode()
+    let CoreMotionManager = CMMotionManager()
+    var impulseCount = 4
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
     override init(size: CGSize) {
         super.init(size: size)
         physicsWorld.contactDelegate = self
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -5.0);
         backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         isUserInteractionEnabled = true
         
-        //adding the background
+        // adding the background
         backgroundNode.size.width = frame.size.width
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
         backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
         addChild(backgroundNode)
+        addChild(foregroundNode)
         
-        //add the player
+        // add the player
         playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
-        playerNode.physicsBody?.isDynamic = true
-        
-        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.isDynamic = false
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 180.0)
         playerNode.physicsBody?.linearDamping = 1.0
         playerNode.physicsBody?.allowsRotation = false
         playerNode.physicsBody?.categoryBitMask = CollisionCategoryPlayer
         playerNode.physicsBody?.contactTestBitMask = CollisionCategoryPowerUpOrbs
         playerNode.physicsBody?.collisionBitMask = 0
-        addChild(playerNode)
+        foregroundNode.addChild(playerNode)
         
-        //add node collide
-        orbNode.position = CGPoint(x: 150.0, y: size.height - 25)
-        orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
-        orbNode.physicsBody?.isDynamic = false
-        orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
-        orbNode.physicsBody?.collisionBitMask = 0
-        orbNode.name = "POWER_UP_ORB"
-        addChild(orbNode)
+        //adding orbNode
+        var orbNodePosition = CGPoint(x: playerNode.position.x, y: playerNode.position.y + 100)
+        for _ in 0...19 {
+            let orbNode = SKSpriteNode(imageNamed: "PowerUp")
+            orbNodePosition.y += 140
+            orbNode.position = orbNodePosition
+            orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
+            orbNode.physicsBody?.isDynamic = false
+            orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+            orbNode.physicsBody?.collisionBitMask = 0
+            orbNode.name = "POWER_UP_ORB"
+            foregroundNode.addChild(orbNode)
+        }
+        orbNodePosition = CGPoint(x: playerNode.position.x + 50, y: orbNodePosition.y)
+        for _ in 0...19 {
+            let orbNode = SKSpriteNode(imageNamed: "PowerUp")
+            orbNodePosition.y += 140
+            orbNode.position = orbNodePosition
+            orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
+            orbNode.physicsBody?.isDynamic = false
+            orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+            orbNode.physicsBody?.collisionBitMask = 0
+            orbNode.name = "POWER_UP_ORB"
+            foregroundNode.addChild(orbNode)
+        }
+
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        playerNode.physicsBody?.applyImpulse(CGVector(dx:0.0, dy: 40.0))
-        
+        if !playerNode.physicsBody!.isDynamic {
+            playerNode.physicsBody?.isDynamic = true
+            
+            CoreMotionManager.accelerometerUpdateInterval = 0.3
+            CoreMotionManager.startAccelerometerUpdates()
+        }
+        if impulseCount > 0 {
+            playerNode.physicsBody!.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+            impulseCount -= 1
+        }
+    }
+    override func update(_ currentTime: TimeInterval){
+        if playerNode.position.y >= 180.0 {
+            backgroundNode.position = CGPoint(x: backgroundNode.position.x, y: -((playerNode.position.y - 180.0)/8))
+            foregroundNode.position = CGPoint(x: foregroundNode.position.x, y: -(playerNode.position.y - 180.0))
+        }
+    }
+    override func didSimulatePhysics() {
+        if let accelerometerData = CoreMotionManager.accelerometerData {
+            playerNode.physicsBody!.velocity =
+                CGVector(dx: CGFloat(accelerometerData.acceleration.x * 380.0),
+                         dy: playerNode.physicsBody!.velocity.dy)
+        }
+        if playerNode.position.x < -(playerNode.size.width / 2) {
+            playerNode.position = CGPoint(x: size.width - playerNode.size.width / 2, y: playerNode.position.y);
+        }
+            else if playerNode.position.x > self.size.width {
+                playerNode.position = CGPoint(x: playerNode.size.width / 2, y: playerNode.position.y);
+            }
+            
+        }
+    deinit {
+        CoreMotionManager.stopAccelerometerUpdates()
     }
 }
 
-extension GameScene: SKPhysicsContactDelegate{
+extension GameScene: SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
-        let nodeB = contact.bodyB.node
-        if nodeB?.name == "POWER_UP_ORBS" {
-            nodeB?.removeFromParent()
+        let nodeB = contact.bodyB.node!
+        if nodeB.name == "POWER_UP_ORB" {
+            
+            impulseCount += 1
+            nodeB.removeFromParent()
         }
     }
 }
+
